e7af00afd4d6448c5389a54619b00f42217697dc
Ali Yıldızöz <aliyildizoz909@gmail.com>
2024-07-07 01:59:25 +0300

    Excel Clean

---
+++ ReportService/ReportService/Helpers/ExcelHelper.cs
@@ -0,0 +1,82 @@
+﻿using Azure;
+using MeterService;
+using OfficeOpenXml.Style;
+using OfficeOpenXml;
+
+namespace ReportService.Helpers
+{
+    public class ExcelHelper
+    {
+        private static readonly string DirPath = @$"{Environment.CurrentDirectory}/wwwroot/reports";
+        public static string CreateExcel(List<MeterReadingDto> meterReadingDtos, string serialNumber)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var filePath = @$"{DirPath}/{serialNumber}.xlsx";
+            var file = new FileInfo(filePath);
+            using ExcelPackage excel = new ExcelPackage(file);
+
+            var workSheet = excel.Workbook.Worksheets.FirstOrDefault(x => x.Name == $"Meter Report({serialNumber})");
+            if (workSheet == null) workSheet = excel.Workbook.Worksheets.Add($"Meter Report({serialNumber})");
+
+            AddHeader(workSheet);
+
+            Fill(workSheet, meterReadingDtos);
+
+            if (File.Exists(filePath)) File.Delete(filePath);
+
+            FileStream objFileStrm = File.Create(filePath);
+            objFileStrm.Close();
+
+            File.WriteAllBytes(filePath, excel.GetAsByteArray());
+            excel.Dispose();
+
+            return filePath;
+
+        }
+
+        private static void AddHeader(ExcelWorksheet workSheet)
+        {
+            workSheet.TabColor = System.Drawing.Color.Black;
+            workSheet.DefaultRowHeight = 12;
+            workSheet.Row(1).Height = 20;
+            workSheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            workSheet.Row(1).Style.Font.Bold = true;
+
+            workSheet.Cells[1, 1].Value = "Number";
+            workSheet.Cells[1, 2].Value = "Id";
+            workSheet.Cells[1, 3].Value = "Serial Number";
+            workSheet.Cells[1, 4].Value = "Reading Time";
+            workSheet.Cells[1, 5].Value = "End Index";
+            workSheet.Cells[1, 6].Value = "Voltage";
+            workSheet.Cells[1, 7].Value = "Current";
+        }
+
+        private static void Fill(ExcelWorksheet workSheet, List<MeterReadingDto> meterReadingDtos, int recordIndex = 2)
+        {
+
+            foreach (var item in meterReadingDtos)
+            {
+                if (item != null)
+                {
+                    workSheet.Cells[recordIndex, 1].Value = (recordIndex - 1).ToString();
+                    workSheet.Cells[recordIndex, 2].Value = item.Id;
+                    workSheet.Cells[recordIndex, 3].Value = item.SerialNumber;
+                    workSheet.Cells[recordIndex, 4].Value = item.ReadingTime.ToDateTime().ToString("d");
+                    workSheet.Cells[recordIndex, 5].Value = item.EndIndex;
+                    workSheet.Cells[recordIndex, 6].Value = item.Voltage;
+                    workSheet.Cells[recordIndex, 7].Value = item.Current;
+                    recordIndex++;
+                }
+            }
+            workSheet.Column(1).AutoFit();
+            workSheet.Column(2).AutoFit();
+            workSheet.Column(3).AutoFit();
+            workSheet.Column(4).AutoFit();
+            workSheet.Column(5).AutoFit();
+            workSheet.Column(6).AutoFit();
+            workSheet.Column(7).AutoFit();
+        }
+
+    }
+}
--- ReportService/ReportService/RabbitMQ/Services/RabbitMQConsumer.cs
+++ ReportService/ReportService/RabbitMQ/Services/RabbitMQConsumer.cs
@@ -10,6 +10,7 @@ using OfficeOpenXml.Style;
 using MeterService;
 using Google.Protobuf.WellKnownTypes;
 using ReportService.gRPC.Services;
+using ReportService.Helpers;
 
 namespace ReportService.RabbitMQ.Services
 {
@@ -51,62 +52,7 @@ namespace ReportService.RabbitMQ.Services
                 var requestModel = JsonSerializer.Deserialize<RabbitMQReportRequestModel>(Encoding.UTF8.GetString(@event.Body.ToArray()));
                 var response = await _metergGrpcService.GetMetersBySerialNumberAsync(new MeterReadingRequest() { SerialNumber = requestModel.SerialNumber });
 
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-
-                var filePath = @$"{Environment.CurrentDirectory}/wwwroot/reports/{requestModel.SerialNumber}.xlsx";
-                var file = new FileInfo(filePath);
-                using ExcelPackage excel = new ExcelPackage(file);
-
-                var workSheet = excel.Workbook.Worksheets.FirstOrDefault(x => x.Name == $"Meter Report({requestModel.SerialNumber})");
-                if (workSheet == null) workSheet = excel.Workbook.Worksheets.Add($"Meter Report({requestModel.SerialNumber})");
-
-
-                workSheet.TabColor = System.Drawing.Color.Black;
-                workSheet.DefaultRowHeight = 12;
-                workSheet.Row(1).Height = 20;
-                workSheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                workSheet.Row(1).Style.Font.Bold = true;
-
-                workSheet.Cells[1, 1].Value = "Number";
-                workSheet.Cells[1, 2].Value = "Id";
-                workSheet.Cells[1, 3].Value = "Serial Number";
-                workSheet.Cells[1, 4].Value = "Reading Time";
-                workSheet.Cells[1, 5].Value = "End Index";
-                workSheet.Cells[1, 6].Value = "Voltage";
-                workSheet.Cells[1, 7].Value = "Current";
-
-
-                int recordIndex = 2;
-
-                foreach (var item in response.MeterReadingDtos)
-                {
-                    if (item != null)
-                    {
-                        workSheet.Cells[recordIndex, 1].Value = (recordIndex - 1).ToString();
-                        workSheet.Cells[recordIndex, 2].Value = item.Id;
-                        workSheet.Cells[recordIndex, 3].Value = item.SerialNumber;
-                        workSheet.Cells[recordIndex, 4].Value = item.ReadingTime.ToDateTime().ToString("d");
-                        workSheet.Cells[recordIndex, 5].Value = item.EndIndex;
-                        workSheet.Cells[recordIndex, 6].Value = item.Voltage;
-                        workSheet.Cells[recordIndex, 7].Value = item.Current;
-                        recordIndex++;
-                    }
-                }
-                workSheet.Column(1).AutoFit();
-                workSheet.Column(2).AutoFit();
-                workSheet.Column(3).AutoFit();
-                workSheet.Column(4).AutoFit();
-                workSheet.Column(5).AutoFit();
-                workSheet.Column(6).AutoFit();
-                workSheet.Column(7).AutoFit();
-
-                if (File.Exists(filePath)) File.Delete(filePath);
-
-                FileStream objFileStrm = File.Create(filePath);
-                objFileStrm.Close();
-
-                File.WriteAllBytes(filePath, excel.GetAsByteArray());
-                excel.Dispose();
+                var filePath = ExcelHelper.CreateExcel(response.MeterReadingDtos.ToList(), requestModel.SerialNumber);
 
                 var report = await _reportContext.ReportRequests.FirstOrDefaultAsync(x => x.Id == requestModel.Id);
                 report.Status = Status.Completed;
